8f01ae3ab22b98079395aa2285004028678f8995
Adiazx14 <adrianvaldes1439@gmail.com>
2021-07-06 14:32:55 -0400

    still trying to deploy lol

--- task_tracker_django_rest/settings.py
+++ task_tracker_django_rest/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["drf-task-tracker.herokuapp.com/"]
 
 
 # Application definition
